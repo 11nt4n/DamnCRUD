b4f80d4bf375feb7736de10d38ac2c81b9275df2
hermanka <hermanka.beta@gmail.com>
2026-02-23 05:20:34 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -3,7 +3,6 @@
         <a type="button" class="btn btn-success create-contact" href="create.php">Add New Contact</a>
         <div class="action float-right">
         <a type="button" class="btn btn-primary" href="profil.php">Profil</a> 
-        <a type="button" class="btn btn-warning" href="vpage.php">VPage</a> 
         <a type="button" class="btn btn-danger" href="logout.php">Sign out</a> 
         </div>                  
     </div>
